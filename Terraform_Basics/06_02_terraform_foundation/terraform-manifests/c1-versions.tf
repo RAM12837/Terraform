77f5a1cae5baf0d5a34a4d38274daaf6ee088d52
RAM12837 <ramkumardamde1432@gmail.com>
2026-08-03 22:52:59 +0000

    Add Terraform configuration files for AWS S3 bucket and outputs

---
+++ Terraform_Basics/06_02_terraform_foundation/terraform-manifests/c1-versions.tf
@@ -0,0 +1,17 @@
+terraform {
+    required_version = "~> 1.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "6.57.1"
+        }
+        random = {
+            source  = "hashicorp/random"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
---
+++ Terraform_Basics/06_02_terraform_foundation/terraform-manifests/c3-outputs.tf
@@ -0,0 +1,8 @@
+output "bucket_name" {
+  value = aws_s3_bucket.demo_bucket.bucket
+
+}
+
+output "bucket_ARN" {
+    value = aws_s3_bucket.demo_bucket.arn
+}
