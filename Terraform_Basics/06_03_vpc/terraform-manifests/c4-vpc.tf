db5ddd4643d7b48088429ef4bf9606ee809306e8
Ramkumar Damde <98808871+RAM12837@users.noreply.github.com>
2026-08-07 05:04:21 +0530

    Add comments for VPC resources in c4-vpc.tf

---
+++ Terraform_Basics/06_03_vpc/terraform-manifests/c4-vpc.tf
@@ -0,0 +1,10 @@
+# Resource-1: VPC
+# Resource-2: Internet Gateway
+# Resource-3: Public Subnets
+# Resource-4: Private Subnets
+# Resource-5: Elastic IP for NAT Gateway
+# Resource-6: NAT Gateway
+# Resource-7: Public Route Table
+# Resource-8: Public Route Table Associate to Public Subnet
+# Resource-9: Private Route Table
+# Resource-10: Private Route Table Association to Private Subnet
