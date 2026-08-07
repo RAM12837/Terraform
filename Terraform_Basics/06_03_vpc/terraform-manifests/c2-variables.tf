d8ecf2251384b671b4724d01f62b11bfaf2ee900
RAM12837 <ramkumardamde1432@gmail.com>
2026-08-07 01:31:45 +0000

    Add VPC and subnet resources with outputs and variable definitions

--- Terraform_Basics/06_03_vpc/terraform-manifests/c2-variables.tf
+++ Terraform_Basics/06_03_vpc/terraform-manifests/c2-variables.tf
@@ -1,5 +1,33 @@
 # 1. Aws Region 
+variable "aws_region" {
+    description = "Aws region to deploy the resources"
+    type = string
+    default = "us-east-1"
+}
+
 # 2. Environment Name 
+variable "env_name" {
+    description = "Environment name to deploy the resources"
+    type = string
+    default = "dev"
+}
 # 3. VPC CIDR
+variable "vpc_cidr" {
+    description = "Vpc CIDR block to deploy the resources"
+    type = string
+    default = "10.0.0.0/16"
+}
 # 4. Tag
+variable "tag" {
+    description = "Tag to deploy the resources"
+    type = map(string)
+    default = {
+        terraform = "true"
+    }
+}
 # 5. subnet new bits
+variable "subnet_new_bites" {
+    description = "Subnet new bits to deploy the resources"
+    type = number
+    default = 8
+}
--- Terraform_Basics/06_03_vpc/terraform-manifests/c3-datasources-and-locals.tf
+++ Terraform_Basics/06_03_vpc/terraform-manifests/c3-datasources-and-locals.tf
@@ -1,3 +1,11 @@
 # Datasource
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 # Local Block
+locals {
+    azs = slice(data.aws_availability_zones.available.names, 0, 3)
+    public_subnets = [for n, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_new_bites, n)]
+    private_subnets = [for n, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_new_bites, n + 10)]
+}
